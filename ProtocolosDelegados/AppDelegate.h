0b6816b5de560ad03f6a7dc9047bbe6cfb4b71c1
Estudiante <mac_alfil_9@Mac-Alfil-9.local>
2014-08-21 18:19:51 -0500

    Initial Commit

---
+++ ProtocolosDelegados/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ProtocolosDelegados
+//
+//  Created by Estudiante on 21/08/14.
+//  Copyright (c) 2014 Diego Zapata Murillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ProtocolosDelegados/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ProtocolosDelegados
+//
+//  Created by Estudiante on 21/08/14.
+//  Copyright (c) 2014 Diego Zapata Murillo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
